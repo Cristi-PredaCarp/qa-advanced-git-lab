9fc1ed071f34c8e750740c78ff7a2ddd130fc64e
Cristi-PredaCarp <77492251+Cristi-PredaCarp@users.noreply.github.com>
2026-09-20 19:50:39 +0300

    feat: Adauga cod Calculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public int aduna(int a, int b) {
+        int bonus = 2; // temporar
+        return a + b + bonus;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Rezultat: " + calc.aduna(5, 10));
+    }
+}
